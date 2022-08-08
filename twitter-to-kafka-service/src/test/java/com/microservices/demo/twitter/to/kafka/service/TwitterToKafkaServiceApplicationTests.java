ba327b4fec035b560f758c571ca1eaa75400275d
gride29 <cezario2000@gmail.com>
2022-08-08 02:14:28 +0200

    First microservice

---
+++ twitter-to-kafka-service/src/test/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.microservices.demo.twitter.to.kafka.service;
+
+import org.junit.jupiter.api.Test;
+
+public class TwitterToKafkaServiceApplicationTests {
+
+    @Test
+    public void contextLoad() {
+
+    }
+}
